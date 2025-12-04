8e8b8d2a77ebcffaca3b6f66fe3aecc5af53a5c3
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2025-12-04 11:01:46 +0100

    Start

--- script.js
+++ script.js
@@ -11,15 +11,25 @@ function updateCanvasSize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  context.fillStyle = 'rgba(255, 0, 0, 0.5)';
-  context.strokeStyle = "red";
   context.lineWidth = 3;
 
+  for (let i = 0; i < 10; i++) {
   context.beginPath();
-  context.moveTo(200,300);
-  context.arc(200, 300, 100, 0.1 * Math.PI, 1.9 * Math.PI);
-  context.lineTo(200, 300);
 
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  context.fillStyle = 'rgba({r}, {g}, {b}, 0.5)';
+  context.strokeStyle = 'rgba({r},{g}, {b}, 1)';
+
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  context.moveTo(x, y);
+  context.arc(x, y, 100, 0.1 * Math.PI, 1.9 * Math.PI);
+  context.lineTo(x, y);
+  
   context.fill();
   context.stroke();
+  }
 }
