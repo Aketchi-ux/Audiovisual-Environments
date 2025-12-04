17ec2f0aa742aaf4077153ae5ecc21ffbbfa7068
Aketchi-ux <mba49068@stud.hs-furtwangen.de>
2025-12-04 10:48:21 +0100

    Start

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ updateCanvasSize();
 function updateCanvasSize() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  context.fillStyle = 'rgba(255, 0, 0, 0.5)';
+  context.strokeStyle = "red";
+  context.lineWidth = 3;
+
+  context.beginPath();
+  context.moveTo(200,300);
+  context.arc(200, 300, 100, 0.1 * Math.PI, 1.9 * Math.PI);
+  context.lineTo(200, 300);
+
+  context.fill();
+  context.stroke();
 }
